c7700f9719e720b33becce637a853ce8f27ff50f
jisenber <isenberg.jacob@yahoo.com>
2018-04-23 19:08:16 -0700

    refactored shaming logic to account for .save not being a promise

--- server.js
+++ server.js
@@ -16,36 +16,42 @@ const cors = require('cors');
 const LocalStrategy = require('passport-local').Strategy;
 //const flash = require('connect-flash');
 const expressSession = require('express-session');
-const MongoStore = require('connect-mongo')(expressSession);
+//const MongoStore = require('connect-mongo')(expressSession);
 
 
 let app = express();
 let PORT = process.env.PORT || 4200;
 
 //define monogo and connect it.
-let MONGODB_URI =  process.env.MONGODB_URI || 'mongodb://localhost/invaders';
+let MONGODB_URI =  'mongodb://localhost/invaders';
 
-mongoose.connect(MONGODB_URI);
-mongoose.Promise = Promise;
+mongoose.Promise = global.Promise;
 
-//passport config
+mongoose.connect(MONGODB_URI)
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error('horseshit', err));
+
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
 //mouting routes and middlware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static('public'));
 app.use(expressSession({
-  secret: process.env.EXPRESS_SES_SECRET || 'keyboard cat',
+  secret: 'keyboard cat',
   resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
+  saveUninitialized: true,
+  cookie: { secure: true }
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+//passport config
+
+
 app.use(morgan('dev'));
 app.use(cors());
 app.use(errorMiddleWare);
