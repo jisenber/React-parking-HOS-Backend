6e18359e8fa58183ab8030e3a05c80f64d8e2f5a
jisenber <isenberg.jacob@yahoo.com>
2018-05-14 18:57:11 -0700

    fixing bug in model

--- model/user.js
+++ model/user.js
@@ -5,7 +5,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 let userSchema = mongoose.Schema({
   username: {type: String, unique: true, required: true},
   password: {type: String, required: true},
-  email: {type: String, require: true},
+  email: {type: String, unique: true, required: true},
   posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'invaders'}]
 });
 
