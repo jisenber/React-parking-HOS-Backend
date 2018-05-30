cda9822965140e7af694056797465ad4c572ba23
jisenber <isenberg.jacob@yahoo.com>
2018-05-29 19:41:45 -0700

    more bug fixing

--- route/invader-routes.js
+++ route/invader-routes.js
@@ -13,6 +13,7 @@ let router = module.exports = new Router();
 
 //submits an invader to mongoDB
 router.post('/submit', jsonParser, (req, res) => {
+  console.log('here is the req body', req.body);
   new Invader(req.body).save()
     .then(invader => {
       let pltAndState = invader.lic_plate.concat(invader.lic_state);
@@ -33,6 +34,7 @@ router.post('/submit', jsonParser, (req, res) => {
 
 function addInvaderPostingCredit(userEmail, imgUrl, res) {
   let query = {email : userEmail};
+  console.log('here is the userEmail: ', userEmail);
   User.findOneAndUpdate(query,
     { '$push': { 'posts': imgUrl } },
     {upsert:true}, function(err, successMsg) {
